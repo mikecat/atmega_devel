72f6e54669f7859b0123b2d4a592aaf22d868cc6
MikeCAT <mikecat@users.noreply.github.com>
2015-01-18 00:22:34 +0900

    implement opcode table lookup: just print name of the functions

--- bin2mnemonic.pl
+++ bin2mnemonic.pl
@@ -4,19 +4,14 @@ use strict;
 
 my $command_list_file="command_list.txt";
 
-my %convert_table={};
+my @convert_table=();
 my $nop = "0000 0000 0000 0000";
 
 open(COMMAND,"< $command_list_file") or die("failed to open the command list\n");
 while(my $line=<COMMAND>) {
 	chomp($line);
 	if($line eq "" || substr($line,0,1) eq "#"){next;}
-	# 命令名を取得する
-	my $name="";
-	$line =~ s/\t([A-Z]+)([ \t])/$name=$1,$2 eq "\t"?"\t\t":"\t"/e;
-	if($name ne "") {
-		$convert_table{$name}=$line;
-	}
+	push(@convert_table,$line);
 }
 close(COMMAND);
 
@@ -55,4 +50,73 @@ while(my $line = <STDIN>) {
 			$input_data[$cur_addr] = $data[4 + $i];
 		}
 	}
+}
+
+my $current_addr = 0;
+for (my $i = 0; $i < @input_data - 1; $i += 2) {
+	if ($input_data[$i] < 0 || $input_data[$i + 1] < 0) {next;}
+	my $current_data = $input_data[$i] | ($input_data[$i + 1] << 8);
+	my $current_bin = &num_to_bintext($current_data);
+	my $matched = 0;
+	if (($i >> 1) != $current_addr) {
+		printf "!org 0x%04X\n", ($i >> 1);
+	}
+	for (my $j = 0; $j < @convert_table; $j++) {
+		my ($data,$inst,$tmpl)=split(/\t/, $convert_table[$j]);
+		my @bin_data=split(/\|/, $data);
+		if (&bintext_match($bin_data[0], $current_bin)) {
+			my $current_bin2 = $current_bin;
+			my $ok = 1;
+			for (my $k = 1; $k < @bin_data; $k++) {
+				if ($i + 2 * $k >= @input_data - 1 ||
+				$input_data[$i + 2 * $k] < 0 || $input_data[$i + 2 * $k + 1] < 0) {
+					$ok = 0;
+					last;
+				}
+				my $this_data = $input_data[$i + 2 * $k] | ($input_data[$i + 2 * $k + 1] << 8);
+				my $this_bin = &num_to_bintext($this_data);
+				if (&bintext_match($bin_data[$k], $this_data)) {
+					$current_bin2.="|".$this_bin;
+				} else {
+					$ok = 0;
+					last;
+				}
+			}
+			if ($ok) {
+				print $inst;
+				print "\n";
+				$i += 2 * (@bin_data - 1);
+				$matched = 1;
+				last;
+			}
+		}
+	}
+	unless ($matched) {
+		printf "!word 0x%04X\n", $current_data;
+	}
+	$current_addr = ($i >> 1) + 1;
+}
+
+sub num_to_bintext {
+	my ($in) = @_;
+	my $out = "";
+	for(my $i=0;$i<16;$i++) {
+		if($i>0 && $i%4==0){$out.=" ";}
+		$out.= (($in>>(15-$i))&1)?1:0;
+	}
+	return $out;
+}
+
+sub bintext_match {
+	my ($in1, $in2) = @_;
+	my ($len1, $len2) = (length($in1), length($in2));
+	for (my $i = 0; $i < $len1 || $i < $len2; $i++) {
+		my $char1 = ($i < $len1) ? substr($in1, $i, 1) : "";
+		my $char2 = ($i < $len2) ? substr($in2, $i, 1) : "";
+		if (($char1 eq "0" || $char1 eq "1") && ($char2 eq "0" || $char2 eq "1") &&
+		$char1 ne $char2) {
+			return 0;
+		}
+	}
+	return 1;
 }
